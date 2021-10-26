a21bd1e070025fc1abb6dd92f8331e75b7bc5b0c
lauraorgmb <92545012+lauraorgmb@users.noreply.github.com>
2021-10-26 17:10:20 +0200

    Update index.py

--- index.py
+++ index.py
@@ -6,7 +6,11 @@ Created on Tue Oct 26 10:02:29 2021
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def ndvi(RED, NIR):
     ndvi = (NIR - RED) / (RED + NIR)
+    plt.imshow(ndvi)
+    plt.show()
+
     return ndvi
