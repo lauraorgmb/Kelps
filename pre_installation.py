e97af19e640290a01a5d8c6a6881c87612b810be
lauraorgmb <92545012+lauraorgmb@users.noreply.github.com>
2021-10-26 15:19:52 +0200

    Add files via upload

---
+++ pre_installation.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 26 15:11:05 2021
+
+@author: 22007604
+"""
+#%% CREATE NEW ENVIRONMENT
+import os
+
+os.system("conda create -n kelp")
+print("""Be careful! The script is now stopped. You have to open Anaconda 
+      interface, activate via the interface Kelp environment and 
+      INSTALL SPYDER! Otherwise, Gdal will not be functional.
+      """)
+
+
+#%% INSTALL LIBRARIES IN THE NEW ENVIRONMENT 
+
+os.system("conda activate kelp")   
+
+os.system("conda install -c conda-forge gdal")
+
+os.system("conda install -c conda-forge matplotlib")
+
+os.system("conda install -c anaconda pandas")
+
+os.system("conda install -c anaconda scikit-learn")
+
