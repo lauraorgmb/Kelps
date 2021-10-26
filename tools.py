0dad1f905de710a9002335012fc5cf06202a0880
lauraorgmb <92545012+lauraorgmb@users.noreply.github.com>
2021-10-26 17:09:18 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -6,6 +6,7 @@ This is a temporary script file.
 """
 import glob
 import os
+from xml.dom import minidom
 from osgeo import gdal, osr
 import numpy.ma as ma
 
@@ -20,7 +21,7 @@ def SAFE2band(SAFE_path, band_nb, resolution):
     band_nb : str
         Number of band selected, such as "B02", "B8A", etc.
     resolution : TYPE
-        Selected resolution (10m or 20m).
+        Selected resolution (10 or 20).
 
     Returns
     -------
@@ -28,13 +29,13 @@ def SAFE2band(SAFE_path, band_nb, resolution):
         Path to selected granule.
 
     """
-    granule_path = SAFE_path + r"/GRANULE" #go to granule path
+    granule_path = os.path.join(SAFE_path, "GRANULE") #go to granule path
     os.chdir(granule_path) #work in granule path
     list_L2A = glob.glob("L2A*") #look for folders begining by "L2A"
-    suffix = list_L2A[0] + r"/IMG_DATA/R" + resolution #precise band's suffix
+    suffix = list_L2A[0] + r"\IMG_DATA\R" + resolution + "m"
     resolution_path = os.path.join(granule_path, suffix)
     os.chdir(resolution_path) #work in the granule path
-    band_suffix = "*" + band_nb + "_" + resolution + ".jp2"
+    band_suffix = "*" + band_nb + "_" + resolution + "m.jp2"
     band_list = glob.glob(band_suffix)
     band_path = os.path.join(granule_path, band_list[0])
     return band_path 
@@ -96,6 +97,15 @@ def array2raster(out_fn, raster_origin, pixel_width, pixel_height, array):
     outband.WriteArray(reversed_arr)
     outband.SetNoDataValue(-9999)
     out_rasterSRS = osr.SpatialReference()
-    out_rasterSRS.ImportFromEPSG(32629) #A CHANGER
+    out_rasterSRS.ImportFromEPSG(32742)
     out_raster.SetProjection(out_rasterSRS.ExportToWkt())
     outband.FlushCache()
+    
+def get_origin(granule_path):
+
+    list_L2A = glob.glob("L2A*") #look for folders begining by "L2A"
+    suffix = os.path.join(list_L2A[0], "MTD_TL.xml")
+    xml_path = os.path.join(granule_path, suffix)
+    mtd_xml = minidom.parse(xml_path)
+    return mtd_xml
+
