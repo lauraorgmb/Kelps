81f727e91773a3b81bebdda52e60da65b0578a0c
lauraorgmb <92545012+lauraorgmb@users.noreply.github.com>
2021-11-15 20:44:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,21 +9,24 @@ import glob
 
 if __name__ == "__main__":
     
-    os.chdir(r"E:\Kelp")
-
-    from tools import SAFE2band, raster2array, array2raster, get_origin
-    from index import ndvi
-    from lxml import etree
+    wd = r"E:\Kelp"
+    os.chdir(wd)
     
-    wd = r"E:\Kelp\Data"
+#%%
+    from tools import SAFE2band, raster2array, array2raster
+    from index import ndvi, kelpDiff, floatAlgae
+    
+#%%
+    
+    data_wd = os.path.join(wd, "Data")
     year_study = ["2018", "2021"]
     
-    os.chdir(wd)
+    os.chdir(data_wd)
     year_list = glob.glob("*") 
     
     for year in year_list:
         if year in year_study:
-            year_fd = os.path.join(wd,year) #fd : folder
+            year_fd = os.path.join(data_wd,year) #fd : folder
             os.chdir(year_fd)
             
             #open image Sentinel-2
@@ -34,41 +37,46 @@ if __name__ == "__main__":
                 #define .SAFE path name
                 SAFE_path = os.path.join(year_fd,S2_safe)
                 granule_path = os.path.join(SAFE_path, "GRANULE")
-                os.chdir(granule_path)
-                list_L2A = glob.glob("L2A*") #fetch folders begining by "L2A"
-                suffix = os.path.join(list_L2A[0], "MTD_TL.xml")
-                xml_path = os.path.join(granule_path, suffix)
-                with open(xml_path) as xml_file:
-                    xml_str = xml_file.read()
-                    ULX = xml_str.find('Tile_Geocoding/Geoposition')
-                    print(ULX)
-                    # https://stackoverflow.com/questions/11351183/how-to-get-xml-tag-value-in-python
-                    # https://www.python101.pythonlibrary.org/chapter31_lxml.html
                 
-
-#%%
                 #define band path name and open raster
                 B4_20m_fn = SAFE2band(SAFE_path, "B04", "20")
                 B4_20m = raster2array(B4_20m_fn)
                 
+                B6_20m_fn = SAFE2band(SAFE_path, "B06", "20")
+                B6_20m = raster2array(B6_20m_fn)
+                
                 B7_20m_fn = SAFE2band(SAFE_path, "B07", "20")
                 B7_20m = raster2array(B7_20m_fn)
                 
+                B8A_20m_fn = SAFE2band(SAFE_path, "B8A", "20")
+                B8A_20m = raster2array(B8A_20m_fn)
+                
+                B11_20m_fn = SAFE2band(SAFE_path, "B11", "20")
+                B11_20m = raster2array(B11_20m_fn)
+                
                 #apply mask
                 
-                #compute index and save as TIF raster in result dir
-                ndvi_20m = ndvi(B7_20m, B4_20m)
-                
-                result_path = os.path.join(year_fd, "results")
-                
-                #define georeference parameters of the tile
-                raster_origin = (600000,4600000)
-                pixel_width = 10
-                pixel_height = 10
-                
-                if not os.exist(result_path):
+                #compute index
+                ndvi_20m = ndvi(B4_20m, B7_20m)
+                kd_20m = kelpDiff(B4_20m, B6_20m)
+                FAI_20m = floatAlgae(B4_20m, B8A_20m, B11_20m)
+
+                #create a folder containing results
+                result_path = os.path.join(year_fd, "Results")
+                if not os.path.exists(result_path):
                     os.mkdir(result_path)
-                os.chdir(result_path)
-                array2raster("S2_NDVI.tif", raster_origin, pixel_width,
-                             pixel_height, ndvi_20m)
+                date_path = os.path.join(result_path, S2_safe[15:17] + 
+                                         "-" + S2_safe[17:19])
+                if not os.path.exists(date_path):
+                    os.mkdir(date_path)
+                
+                #save index products as rasters
+                out_fn = os.path.join(date_path, 'S2_ndvi_20m.tif')
+                array2raster(out_fn, 20, ndvi_20m, granule_path)
+                
+                out_fn = os.path.join(date_path, 'S2_kd_20m.tif')
+                array2raster(out_fn, 20, kd_20m, granule_path)
+                
+                out_fn = os.path.join(date_path, 'S2_FAI_20m.tif')
+                array2raster(out_fn, 20, FAI_20m, granule_path)
                 
